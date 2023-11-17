06e4206949d6be448f8f36e1b9f1f60c3ea8fcb3
MatrixMe00 <safosah00@gmail.com>
2023-11-17 12:10:04 +0000

    Update foot.php

--- includes/foot.php
+++ includes/foot.php
@@ -2,7 +2,7 @@
  <script src="assets/vendors/js/vendor.bundle.base.js"></script>
  <!-- endinject -->
  <!-- Plugin js for this page -->
- <!-- <script src="assets/vendors/chart.js/Chart.min.js"></script> -->
+ <script src="assets/vendors/chart.js/Chart.min.js"></script>
  <!-- End plugin js for this page -->
  <!-- inject:js -->
  <script src="assets/js/off-canvas.js"></script>
